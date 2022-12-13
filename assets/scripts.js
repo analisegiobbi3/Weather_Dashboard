8912182ffdd033657f46bf84836421c83723f9c6
Analise <analise.giobbi@gc.com>
2022-12-13 11:25:00 -0500

    attempt to pull location api

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,40 +1,70 @@
-
-var currentDay = $("#currentDay")
 var today = dayjs();
+var formEl = $("#cityInput");
+var btnEl = $(".btn");
+var inputEl = $("#locationInput");
+var key = "9c343e5b82dbaddad935bd1ba04a1d88";
 
-currentDay.text(today.format('MMM D, YYYY'));
 
 //use data-location data attribute to create buttons for your searches so you can clikc them again and bring up the information review one has a good example
 
 // create function that takes your input and stores them in a list. This is where you can add that data attriute so they can then be passed through again
-
-function searchLocation(event, location){
+var locationFormHandler = function (event){
     event.preventDefault();
+    var locationInput = inputEl.value.trim();
+    localStorage.setItem("location", locationInput);
+    if (locationInput){
+        getlocation(locationInput);
+        inputEl.value = '';
 
-
+    }else{
+        alert("Please enter a location")
+    }
 }
 
-var getWeather = function (){
-    // you'll pass your search location through this to grab the weather data. You will need to build the weather elements to store the data
-    // you can use the structure you already have
-    var apiURL ='api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=9c343e5b82dbaddad935bd1ba04a1d88'
-    var locationURL = 
-    //you will add the api URL above
 
-    fetch(apiURL).then(function(response){
+var getlocation = function (location){
+    var locationURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&appid=' + key;
+    fetch(locationURL).then(function(response){
         if (response.ok){
-            console.log(response);
+            console.log(response)
             response.json().then(function(data){
                 console.log(data)
-                //you'll create a function here that dispalys the data and you'll call your inputs 
             })
-
         }else{
-            alert("error " + response.statusText);
+            alert('Error' + response.statusText)
         }
-
     })
+    .catch(function (error) {
+        alert('Unable to connect to location services');
+    });
 }
+
+formEl.addEventListener('submit', locationFormHandler);
+
+
+
+// var getWeather = function (lat, lon){
+//     // you'll pass your search location through this to grab the weather data. You will need to build the weather elements to store the data
+//     // you can use the structure you already have
+//     var apiURL ='api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon +'&appid=' + key;
+  
+
+//     fetch(apiURL).then(function(response){
+//         if (response.ok){
+//             console.log(response);
+//             response.json().then(function(data){
+//                 console.log(data)
+//                 //you'll create a function here that dispalys the data and you'll call your inputs 
+//             })
+
+//         }else{
+//             alert("error " + response.statusText);
+//         }
+
+//     })
+// }
 
 // you'll add an event handler here to handle the search location function 
 //you might need a second event handler to handle the locations that were previously searched
+
+
