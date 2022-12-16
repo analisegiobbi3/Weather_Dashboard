15e3ac16f1315b37c09361dbdd6ba8a86d986c99
Analise <analise.giobbi@gc.com>
2022-12-16 14:12:24 -0500

    added icons to five day

--- assets/scripts.js
+++ assets/scripts.js
@@ -63,6 +63,8 @@ var getWeather = function (lat, lon){
             var tempK = data.list[0].main.temp;
             var wind = data.list[0].wind.speed;
             var humidity = data.list[0].main.humidity;
+            // var weatherIcon = data.list[0].weather[0].icon;
+            // var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempF = parseInt(1.8*(tempK-273)+32);
             todayWeatherInterface(city, tempF, wind, humidity)
         })
@@ -72,7 +74,7 @@ var getWeather = function (lat, lon){
     });
 }
 
-function todayWeatherInterface(city, temp, wind, humid){
+function todayWeatherInterface(city, temp, wind, humid, icon){
     var todayHeader = city + ": " + todayDate;
     var todayTemp = "Temp: " + temp + " °F";
     var todayWind = "Wind: " + wind + " %";
@@ -81,6 +83,7 @@ function todayWeatherInterface(city, temp, wind, humid){
     $("#todayTemp").text(todayTemp);
     $("#todayWind").text(todayWind);
     $("#todayHumidity").text(todayHumidity);
+    // $("#weatherIcon1").attr('src', icon)
 }
 
 var getfiveDayWeatherForcast = function(lat, lon){
@@ -88,7 +91,9 @@ var getfiveDayWeatherForcast = function(lat, lon){
     fetch(fiveDay).then(function(response){
         response.json().then(function(data){
             //Day 2
-            var dayHeader2 = todayDate
+            var dayHeader2 = today.add(1, 'day')
+            var weatherIcon = data.list[8].weather[0].icon;
+            var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempK2 = data.list[8].main.temp;
             var wind2 = data.list[8].wind.speed;
             var humidity2 = data.list[8].main.humidity;
@@ -97,20 +102,26 @@ var getfiveDayWeatherForcast = function(lat, lon){
             $("#temp2").text("Temp: " + tempF2 + " °F");
             $("#wind2").text("Wind: " + wind2 + " %");
             $("#hum2").text("Humidity: " + humidity2 + " MPH");
+            $("#weatherIcon2").attr('src', weatherIconURL)
        
             //Day 3
-            var dayHeader3 = todayDate
+            var dayHeader3 = today.add(2, 'day')
+            var weatherIcon = data.list[16].weather[0].icon;
+            var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempK3 = data.list[16].main.temp;
             var wind3 = data.list[16].wind.speed;
             var humidity3 = data.list[16].main.humidity;
             var tempF3 = parseInt(1.8*(tempK3-273)+32);
-            $("#daythree").text(dayHeader2);
+            $("#daythree").text(dayHeader3);
             $("#temp3").text("Temp: " + tempF3 + " °F");
             $("#wind3").text("Wind: " + wind3 + " %");
             $("#hum3").text("Humidity: " + humidity3 + " MPH");
+            $("#weatherIcon3").attr('src', weatherIconURL)
 
             //Day 4
-            var dayHeader4 = todayDate
+            var dayHeader4 = today.add(3, 'day')
+            var weatherIcon = data.list[24].weather[0].icon;
+            var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempK4 = data.list[24].main.temp;
             var wind4 = data.list[24].wind.speed;
             var humidity4 = data.list[24].main.humidity;
@@ -119,9 +130,12 @@ var getfiveDayWeatherForcast = function(lat, lon){
             $("#temp4").text("Temp: " + tempF4 + " °F");
             $("#wind4").text("Wind: " + wind4 + " %");
             $("#hum4").text("Humidity: " + humidity4 + " MPH");
+            $("#weatherIcon4").attr('src', weatherIconURL)
 
             //Day 5
-            var dayHeader5 = todayDate
+            var dayHeader5 = today.add(4, 'day')
+            var weatherIcon = data.list[32].weather[0].icon;
+            var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempK5 = data.list[32].main.temp;
             var wind5 = data.list[32].wind.speed;
             var humidity5 = data.list[32].main.humidity;
@@ -130,9 +144,12 @@ var getfiveDayWeatherForcast = function(lat, lon){
             $("#temp5").text("Temp: " + tempF5 + " °F");
             $("#wind5").text("Wind: " + wind5 + " %");
             $("#hum5").text("Humidity: " + humidity5 + " MPH");
+            $("#weatherIcon5").attr('src', weatherIconURL)
 
             //Day 6
-            var dayHeader6 = todayDate
+            var dayHeader6 = today.add(5, 'day') 
+            var weatherIcon = data.list[39].weather[0].icon;
+            var weatherIconURL = 'https://openweathermap.org/img/wn/'+weatherIcon+'@2x.png';
             var tempK6 = data.list[39].main.temp;
             var wind6 = data.list[39].wind.speed;
             var humidity6 = data.list[39].main.humidity;
@@ -141,10 +158,13 @@ var getfiveDayWeatherForcast = function(lat, lon){
             $("#temp6").text("Temp: " + tempF6 + "°F");
             $("#wind6").text("Wind: " + wind6 + "%");
             $("#hum6").text("Humidity: " + humidity6 + "MPH");
+            $("#weatherIcon6").attr('src', weatherIconURL)
         })
     })
 
 }
+
+
 
 // you'll add an event handler here to handle the search location function 
 //you might need a second event handler to handle the locations that were previously searched
